936c3bde8bc8e928471e5c5bdb02e03f7d3532e4
mattjax16 <mnbass21@colby.edu>
2021-08-03 14:49:44 -0400

    reformat

--- LRCmediaUploader/routes.py
+++ LRCmediaUploader/routes.py
@@ -46,17 +46,17 @@ def signup():
 @app.route("/")
 @app.route("/login",methods = ['GET','POST'])
 def login():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('home'))
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(email=form.email.data).first()
-    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
-    #         login_user(user, remember=form.remember.data)
-    #         next_page = request.args.get('next')
-    #         return redirect(next_page) if next_page else redirect(url_for('home'))
-    #     else:
-    #         flash('Login Unsuccessful. Please check email and password', 'danger')
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 #for uploading
